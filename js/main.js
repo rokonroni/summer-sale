f9d902f8fb9cad3e2a098195ea4120a995533d05
rokonroni <rokonuzzamanroni@gmail.com>
2023-08-20 18:42:18 +0600

    Coupon Funcion Done

--- js/main.js
+++ js/main.js
@@ -24,8 +24,40 @@ for (const div of allDiv) {
         productContainer.appendChild(p);
     
         const totalPrice = getNumber('total-price');
+        
         const total = totalPrice + productPriceFloat;
         const totalMoney = total.toFixed(2);
-        setValue ('total-price', totalMoney)
+        setValue ('total-price', totalMoney);
+        setValue ('total', totalMoney);
+        
+        const totalPay = parseFloat(totalMoney);
+        const couponBtn = document.getElementById('coupon-btn');
+        const makePayBtn = document.getElementById('make-pay');
+        if(totalPay >= 0){
+            makePayBtn.removeAttribute('disabled')
+        }
+        if (totalPay >= 200)(
+            couponBtn.removeAttribute('disabled')
+        )
+        
     })
 }
+
+document.getElementById('coupon-btn').addEventListener('click', function(){
+    const couponInput = document.getElementById('coupon');
+    const coupon = couponInput.value;
+    console.log(coupon)
+    if (coupon === "SELL200") {
+        const totalPrice = getNumber('total-price');
+    const discountPrice = (20/100)*totalPrice;
+    const disPrice = discountPrice.toFixed(2);
+    setValue ('discount-price', disPrice);
+    const totalMoney = totalPrice - parseFloat(disPrice);
+    const money = totalMoney.toFixed(2);
+    setValue ('total', money)
+    }
+    else {
+        alert ('Please input a valid Coupon')
+    }
+})
+
