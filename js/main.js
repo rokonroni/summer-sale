659d317c7038d88aac915000cf09654d9054bbf3
rokonroni <rokonuzzamanroni@gmail.com>
2023-08-21 21:37:28 +0600

    Error Fix

--- js/main.js
+++ js/main.js
@@ -9,40 +9,66 @@ function setValue (inputId, value){
     const total = document.getElementById(inputId);
     total.innerText = value
 }
-const allDiv = document.querySelectorAll('.card');
-for (const div of allDiv) {
-    div.addEventListener('click',function(e){
-        const getName= e.target.childNodes[3].innerText;
-        const getPrice = e.target.childNodes[5].innerText.split(' ')[0];
+// const allDiv = document.querySelectorAll('.card');
+// for (const div of allDiv) {
+//     div.addEventListener('click',function(e){
+//         const getName= e.target.childNodes[3].innerText;
+//         const getPrice = e.target.childNodes[5].innerText.split(' ')[0];
+//         const floatPrice =parseFloat(getPrice);
+//         const productPrice = floatPrice.toFixed(2);
+//         const productPriceFloat = parseFloat(productPrice);
+//         const productContainer = document.getElementById('product-container');
+//         const p = document.createElement('p')
+//         p.innerText = (count+=1)+ ". " + getName;
+//         productContainer.appendChild(p);
+//         const totalPrice = getNumber('total-price');
+//         const total = totalPrice + productPriceFloat;
+//         const totalMoney = total.toFixed(2);
+//         setValue ('total-price', totalMoney);
+//         setValue ('total', totalMoney);
+//         const totalPay = parseFloat(totalMoney);
+//         const couponBtn = document.getElementById('coupon-btn');
+//         const makePayBtn = document.getElementById('make-pay');
+//         if(totalPay >= 0){
+//             makePayBtn.removeAttribute('disabled') 
+//         }
+//         if (totalPay >= 200)(
+//             couponBtn.removeAttribute('disabled')
+//         )
+//     })
+// }
+const images = document.querySelectorAll('.card');
+for (const img of images) {
+    img.addEventListener('click',function(e){
+        const getName= e.target.parentNode.childNodes[3].childNodes[3].innerText;
+        console.log(getName)
+        const getPrice = e.target.parentNode.childNodes[3].childNodes[5].innerText.split(' ')[0];
         const floatPrice =parseFloat(getPrice);
         const productPrice = floatPrice.toFixed(2);
         const productPriceFloat = parseFloat(productPrice);
-    
         const productContainer = document.getElementById('product-container');
         const p = document.createElement('p')
         p.innerText = (count+=1)+ ". " + getName;
         productContainer.appendChild(p);
-    
         const totalPrice = getNumber('total-price');
-        
         const total = totalPrice + productPriceFloat;
         const totalMoney = total.toFixed(2);
         setValue ('total-price', totalMoney);
         setValue ('total', totalMoney);
-        
         const totalPay = parseFloat(totalMoney);
         const couponBtn = document.getElementById('coupon-btn');
         const makePayBtn = document.getElementById('make-pay');
         if(totalPay >= 0){
-            makePayBtn.removeAttribute('disabled')
-            
+            makePayBtn.removeAttribute('disabled') 
         }
         if (totalPay >= 200)(
             couponBtn.removeAttribute('disabled')
         )
-        
     })
 }
+
+
+
 
 document.getElementById('coupon-btn').addEventListener('click', function(){
     const couponInput = document.getElementById('coupon');
@@ -62,7 +88,6 @@ document.getElementById('coupon-btn').addEventListener('click', function(){
         alert ('Please input a valid Coupon')
     }
 })
-
 document.getElementById('go-home').addEventListener('click',function(){
     const defultValue = '0.00';
     setValue ('total-price', defultValue);
@@ -72,14 +97,12 @@ document.getElementById('go-home').addEventListener('click',function(){
     while (productContainer.firstChild) {
         productContainer.removeChild(productContainer.firstChild);
     }
-
     const makePayBtn = document.getElementById('make-pay');
     const totalPrice = getNumber('total-price');
     console.log(totalPrice)
     const couponBtn = document.getElementById('coupon-btn');
     if(totalPrice == 0){
-        makePayBtn.setAttribute('disabled','')
-        
+        makePayBtn.setAttribute('disabled','')  
     }
     if (totalPrice < 200)(
         couponBtn.setAttribute('disabled','')
